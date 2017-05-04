f5ff9e30d2b7899f137fa81a37b1e3761c11ea01
Username-Failed <marcuslyngbyekolbe@gmail.com>
2017-05-04 13:44:34 +0200

    Add files via upload

--- votestop.php
+++ votestop.php
@@ -23,10 +23,22 @@ if ($dbc = mysql_connect(DBHOST, DBUSER, DBPW)) {
     exit();
 }
 
+$votingResult = mysql_query("SELECT * FROM voting_rounds WHERE in_progress = 1 ORDER BY id DESC LIMIT 1");
+
+$idArray = array();
+$nameArray = array();
+
+while($row = mysql_fetch_array($votingResult)) {
+    $idArray[] = $row["id"];
+    $nameArray[] = $row["name"];
+}
+
+$voteResult = mysql_query("SELECT * FROM vote WHERE voting_id = " . $idArray['0']);
+
 $sql = "UPDATE voting_rounds SET in_progress = 0 WHERE in_progress = 1";
 mysql_query($sql);
 
-$result_json = array('color' => 'purple', 'message' => 'The voting round have been stopped', 'notify' => 'false', 'message_format' => 'text');
+$result_json = array('color' => 'purple', 'message' => 'The voting round called "' . $nameArray['0'] . '" have been stopped\nNumber of votes: ' . mysql_num_rows($voteResult) . '\nAverage: ', 'notify' => 'false', 'message_format' => 'text');
 //headers for not caching the results
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, Jul 26 1997 05:00:00 GMT');
